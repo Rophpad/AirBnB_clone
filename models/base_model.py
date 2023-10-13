b81c2ac950974695d0f48934a185a8cf272f02ed
Rophpad <rophenp@gmail.com>
2023-10-13 11:25:29 +0100

    User class

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """defines all common attributes/methods for other classes"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -9,7 +10,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         from models.__init__ import storage
-        """class initialization"""
+        """Class initialization"""
         if kwargs and len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -22,10 +23,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """prints the class nicely"""
+        return ('[{}] ({}) {}'.
+                format(self.__class__.__name__, self.id, self.__dict__))
+        """
         result = []
         name = "[" + __class__.__name__ + "]"
         result.append(name)
@@ -40,17 +44,23 @@ class BaseModel:
         result.append(Id)
         result.append(Dict)
         return " ".join(result)
+        """
+
+    def __repr__(self):
+        """String representation"""
+        return (self.__str__())
 
     def save(self):
-        from models.__init__ import storage
+        # from models.__init__ import storage
         """updates the public instance attribute
         updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all
         keys/values of __dict__ of the instance"""
+        """
         Dict = self.__dict__
         Dict['__class__'] = __class__.__name__
         value = Dict.get('created_at')
@@ -59,4 +69,10 @@ class BaseModel:
         value = Dict.get('updated_at')
         value = value.strftime("%Y-%m-%dT%H:%M:%S.%f")
         Dict['updated_at'] = value
+        return Dict
+        """
+        Dict = self.__dict__.copy()
+        Dict["created_at"] = self.created_at.isoformat()
+        Dict["updated_at"] = self.updated_at.isoformat()
+        Dict["__class__"] = self.__class__.__name__
         return Dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,14 +2,17 @@
 """serializes instances to a JSON file and deserializes
 JSON file to instances"""
 import json
-from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
 
 
 class FileStorage:
     """serializes instances to a JSON file and
     deserializes JSON file to instances"""
+
     __file_path = "file.json"
-    __objects = dict()
+    __objects = {}
+    all_class = {"BaseModel": BaseModel, "User": User}
 
     def all(self):
         """returns the dictionary __objects"""
@@ -17,27 +20,27 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        key = obj.__class__.__name__ + '.' + obj.id
-        self.__objects[key] = obj
+        if obj:
+            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        with open(self.__file_path, 'w') as f:
-            obj = {key: obj.to_dict() for key, obj in self.__objects.items()}
-            json.dump(obj, f)
+        objDict = {}
+        for key, obj in self.__objects.items():
+            objDict[key] = obj.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(objDict, f)
 
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON file
         (__file_path) exists ; otherwise, do nothing. If the
         file doesn’t exist"""
-        from models.base_model import BaseModel
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as f:
+            with open(self.__file_path, 'r', encoding='UTF-8') as f:
                 data = json.load(f)
                 for key, value in data.items():
-                    class_name, obj_id = key.split('.')
-                    Dict = {k: v for k, v in value.items() if k != '__class__'}
-                    obj = BaseModel(**Dict)
+                    obj = self.all_class[value['__class__']](**value)
                     self.__objects[key] = obj
         except FileNotFoundError:
             pass
