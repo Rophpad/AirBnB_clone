8bcd0ae6c37f6726a1e5bcb43954de64c31ea871
Rophpad <rophenp@gmail.com>
2023-10-14 13:26:32 +0100

    count method implemented

--- console.py
+++ console.py
@@ -118,7 +118,12 @@ class HBNBCommand(cmd.Cmd):
 
     def do_count(self, arg):
         """Retrieve the number of instances of a class"""
-        # Code
+        args = parse(arg)
+        count = 0
+        for obj in storage.all().values():
+            if argl[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
 
 
 if __name__ == "__main__":
